ce49fc19f956b0daf9ce5cc67ec85b219b7798be
Maik Fr?be <kibi9872@webis4.medien.uni-weimar.de>
2025-04-17 23:55:25 +0200

    mf

--- lsr_benchmark/__init__.py
+++ lsr_benchmark/__init__.py
@@ -3,12 +3,17 @@ import json
 from pathlib import Path
 from lsr_benchmark.corpus.corpus_subsampling import create_subsample
 from trectools import TrecRun
+import ir_datasets
 
 @click.command()
 @click.argument('directory', type=Path)
 def main(directory):
     config = json.loads((directory/"config.json").read_text())
     subsample = create_subsample(config["runs"], config["ir-datasets-id"], config["subsample_depth"], directory)
+    docs_store = ir_datasets.load(config["ir-datasets-id"]).docs_store()
+
+    for doc in subsample:
+        docs_store.get(doc)
 
     print(directory)
 
