38f5a66e4fb11d31fea106e1359867014758843f
Maik Fröbe <maik.froebe@uni-jena.de>
2025-06-02 12:54:17 +0200

    improve irds integration

--- lsr_benchmark/__init__.py
+++ lsr_benchmark/__init__.py
@@ -2,12 +2,13 @@ __version__ = "0.0.1"
 import click
 import json
 from pathlib import Path
-from lsr_benchmark.ir_datasets import ensure_corpus_is_extracted
+from lsr_benchmark.ir_datasets import ensure_corpus_is_extracted, build_dataset_from_local_cache
 from lsr_benchmark.corpus import materialize_corpus, materialize_truths, materialize_inputs
 
 
 def load(ir_datasets_id: str):
     ensure_corpus_is_extracted(ir_datasets_id)
+    return build_dataset_from_local_cache(ir_datasets_id)
 
 @click.group()
 def main():
--- lsr_benchmark/ir_datasets.py
+++ lsr_benchmark/ir_datasets.py
@@ -1,9 +1,69 @@
 from pathlib import Path
 import zipfile
+from tira.check_format import QueryProcessorFormat, JsonlFormat
+from ir_datasets.formats import BaseDocs, TrecQrels, BaseQueries, GenericQuery
+from ir_datasets.datasets.base import Dataset
+from typing import List, NamedTuple
 
 MAPPING_OF_DATASET_IDS = {
     "clueweb09/en/trec-web-2009": "data/trec-18-web"
 }
+
+
+class Segment(NamedTuple):
+    offset_start: int
+    offset_end: int
+    text: str
+
+class LsrBenchmarkDocument(NamedTuple):
+    doc_id: str
+    segments: List[Segment]
+
+    @staticmethod
+    def _from_json(json_doc):
+        segments = [Segment(int(i["start"]), int(i["end"]), i["text"]) for i in json_doc["segments"]]
+        return LsrBenchmarkDocument(json_doc["doc_id"], segments)
+
+class LsrBenchmarkQueries(BaseQueries):
+    def __init__(self, queries):
+       self.__queries = queries
+
+    def queries_iter(self):
+        for l in self.__queries:
+            yield l
+
+    @staticmethod
+    def _from_file(corpus_file):
+        queries = [GenericQuery(i["qid"], i["query"]) for i in QueryProcessorFormat().all_lines(corpus_file)]
+        return queries
+
+class LsrBenchmarkDocuments(BaseDocs):
+    def __init__(self, docs):
+       self.__docs = docs
+
+    def docs_iter(self):
+        for l in self.__docs:
+            yield LsrBenchmarkDocument._from_json(l)
+
+    @staticmethod
+    def _from_file(corpus_file):
+        reader = JsonlFormat()
+        reader.apply_configuration_and_throw_if_invalid({"required_fields": ["doc_id", "segments"], "max_size_mb": 2500})
+        docs = reader.all_lines(corpus_file)
+        return LsrBenchmarkDocuments(docs)
+
+class LsrBenchmarkDataset(Dataset):
+    def __init__(self, docs=None, queries=None, qrels=None, documentation=None):
+        if queries:
+            queries = LsrBenchmarkQueries._from_file(queries)
+
+        if qrels:
+            qrels = TrecQrels(qrels, {0: 'Not Relevant', 1: 'Relevant'})
+
+        if docs:
+            docs = LsrBenchmarkDocuments._from_file(docs)
+
+        super().__init__(docs, queries, qrels, documentation)
 
 def base_dir(ir_datasets_id: str):
     if ir_datasets_id not in MAPPING_OF_DATASET_IDS:
@@ -26,6 +86,14 @@ def extract_zip(zip_file: Path, target_directory: Path):
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         target_directory.mkdir(parents=True, exist_ok=True)
         zip_ref.extractall(target_directory)
+
+def build_dataset_from_local_cache(ir_datasets_id: str):
+    docs = inputs_dir(ir_datasets_id) / "corpus.jsonl.gz"
+    queries = inputs_dir(ir_datasets_id) / "queries.jsonl"
+    qrels = truths_dir(ir_datasets_id) / "qrels.txt"
+
+    return LsrBenchmarkDataset(docs, queries, qrels)
+
 
 def ensure_corpus_is_extracted(ir_datasets_id: str):
     d = base_dir(ir_datasets_id)
