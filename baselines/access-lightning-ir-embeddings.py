b3c9a88e58ca60749ffd1cf53cbb11cd5858a91a
Maik Fröbe <maik.froebe@uni-jena.de>
2025-06-04 12:55:50 +0200

    mf

--- baselines/access-lightning-ir-embeddings.py
+++ baselines/access-lightning-ir-embeddings.py
@@ -8,7 +8,9 @@ lsr_benchmark.register_to_ir_datasets()
 dataset = ir_datasets.load(f"lsr-benchmark/clueweb09/en/trec-web-2009")
 
 for query in dataset.queries_iter(embedding="naver/splade-v3", passage_aggregation="first-passage"):
-    print(query.query_id, query.embedding)
+    print("query:",query.query_id, query.embedding)
+    break
 
-# for doc in dataset.docs_iter(embedding="naver/splade-v3", passage_aggregation="first-passage"):
-#     print(doc.doc_id, doc.embedding)
+for doc in dataset.docs_iter(embedding="naver/splade-v3", passage_aggregation="first-passage"):
+    print("doc:", doc.doc_id, doc.embedding)
+    break
