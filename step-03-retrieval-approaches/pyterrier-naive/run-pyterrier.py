cea8b443d1a0e455079a39c8cd3d28e2b6887d98
Maik Fröbe <maik.froebe@uni-jena.de>
2025-09-30 09:01:34 +0200

    mf

--- step-03-retrieval-approaches/pyterrier-naive/run-pyterrier.py
+++ step-03-retrieval-approaches/pyterrier-naive/run-pyterrier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import lsr_benchmark
 import click
-from tirex_tracker import tracking, ExportFormat
+from tirex_tracker import tracking, ExportFormat, register_metadata
 from tqdm import tqdm
 import pyterrier as pt
 from pathlib import Path
@@ -28,6 +28,7 @@ def main(dataset, output, retrieval, k):
     ir_dataset = ir_datasets.load(f"lsr-benchmark/{dataset}")
     ensure_pyterrier_is_loaded(boot_packages=())
 
+    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"pyterrier-naive-{retrieval.lower()}-top-{k}"})
     documents = [{"docno": i.doc_id, "text": i.default_text()} for i in ir_dataset.docs_iter()]
 
     with tracking(export_file_path=output / "index-metadata.yml", export_format=ExportFormat.IR_METADATA):
