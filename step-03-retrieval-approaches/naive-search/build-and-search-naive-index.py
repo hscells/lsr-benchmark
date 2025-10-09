b729b1c5a36ef35a989ee97a556c8ce6162e139d
Cosimo Rulli <cosimorulli@gmail.com>
2025-10-09 16:06:53 +0000

    cosmetics and typos fixing

--- step-03-retrieval-approaches/naive-search/build-and-search-naive-index.py
+++ step-03-retrieval-approaches/naive-search/build-and-search-naive-index.py
@@ -23,9 +23,9 @@ def main(dataset, embedding, output, k):
     lsr_benchmark.register_to_ir_datasets(dataset)
     ir_dataset = ir_datasets.load(f"lsr-benchmark/{dataset}")
     seismic_dataset = SeismicDataset()
-    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"seismic-{embedding.replace('/', '-')}-{k}"})
+    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"naive_search-{embedding.replace('/', '-')}-{k}"})
     
-    for (doc_id, tokens, values) in tqdm(ir_dataset.doc_embeddings(model_name=embedding), "create seismic dataset"):
+    for (doc_id, tokens, values) in tqdm(ir_dataset.doc_embeddings(model_name=embedding), "create seismic dataset for naive search"):
         seismic_dataset.add_document(doc_id, tokens, values)
 
     with tracking(export_file_path=output / "index-metadata.yml", export_format=ExportFormat.IR_METADATA, ):
@@ -47,7 +47,7 @@ def main(dataset, embedding, output, k):
         for ranking_for_query in results:
             rank = 1
             for qid, score, docno in ranking_for_query:
-                f.write(f"{qid} Q0 {docno} {rank} {score} seismic\n")
+                f.write(f"{qid} Q0 {docno} {rank} {score} naive_search\n")
                 rank += 1
 
 if __name__ == "__main__":
